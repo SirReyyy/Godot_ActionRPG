16636abaaed12a7a0f8393acd3c2110ce9494680
Rey Oronos, Jr <rey.oronos.jr@gmail.com>
2022-12-01 13:20:31 +0800

    1/12/22 - Hitboxes and Hurtboxes
    - Created reusable scenes for Hotbox and Hurtbox
    - Applied hurtbox on grass
    - Applied hitbox on player sword based on attack direction
    - Created pivot for hitbox rotation
    - Updated Collision Layer settings for layer and mask

--- scripts/world_scripts/Grass.gd
+++ scripts/world_scripts/Grass.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-
-func _process(delta):
-	if Input.is_action_just_pressed("player_attack"):
-		var GrassEffect_Scene = load("res://scenes/effect/Grass_Effect.tscn")
-		var grassEffect_Inst = GrassEffect_Scene.instance() 
+func create_grasseffect():
+	var GrassEffect_Scene = load("res://scenes/effect/Grass_Effect.tscn")
+	var grassEffect_Inst = GrassEffect_Scene.instance() 
 		
-		var World_Scene = get_tree().current_scene
-		World_Scene.add_child(grassEffect_Inst)
-		grassEffect_Inst.global_position = global_position
-		
-		queue_free()
+	var World_Scene = get_tree().current_scene
+	World_Scene.add_child(grassEffect_Inst)
+	grassEffect_Inst.global_position = global_position
 	
-	# -- end of _process -- #
+	# -- end of create_grasseffect -- #
+
+func _on_Hurtbox_area_entered(area):
+	create_grasseffect()
+	queue_free()
+	
+	# -- end of _on_Hurtbox_area_entered -- #
